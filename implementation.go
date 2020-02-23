0d9d199a12b45fa56d8a9aee8082b3981d87a7a7
Iluysha <ilua.mix12@gmail.com>
2020-02-23 21:14:27 +0200

    Add implementation

---
+++ implementation.go
@@ -0,0 +1,39 @@
+package lab1
+ 
+import (
+    "strings"
+    "regexp"
+    "errors"
+)
+
+
+func PostfixToInfix(text string) (string, error) {
+
+    var stack []string
+	var s1 string
+	var s2 string
+	var temp string
+	numCount := 0
+	opCount := 0
+    for _, tok := range strings.Fields(text) {
+		if (tok == "*" || tok == "/" || tok == "^" || tok == "+" || tok == "-") {
+			opCount++
+			if (numCount > opCount) {
+				s1, stack = stack[len(stack)-1], stack[:len(stack)-1]
+				s2, stack = stack[len(stack)-1], stack[:len(stack)-1]
+				if tok == strings.Fields(text)[len(strings.Fields(text)) - 1] {
+					temp = s2 + " " + tok + " " + s1
+				} else {
+					temp = "(" + s2 + " " + tok + " " + s1 + ")"
+				}
+				stack = append(stack, temp)
+			} else {
+				return "", errors.New("Incorrect expression")
+			}
+		} else {
+			numCount++
+			stack = append(stack, tok)
+		}
+    }
+	return stack[0], nil
+}
