9522ec472251646ccb911d950d56f7c2472cdb0f
Iluysha <ilua.mix12@gmail.com>
2020-02-23 23:22:28 +0200

    Fix main

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -4,7 +4,6 @@ import (
     "fmt"
 	"bufio"
 	"os"
-	"errors"
 	lab1 "github.com/Tornado9966/Lab1_Go"
 )
 
@@ -14,7 +13,7 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
     
-	if res, err := PostfixToInfix(text); err != nil {
+	if res, err := lab1.PostfixToInfix(text); err != nil {
 		fmt.Println(err.Error())
 	}else {
 		fmt.Println("\nThe infix expression is ", res)
